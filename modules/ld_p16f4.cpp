ccf15c064cb26e65b12efdc7448f0b6a60b938e8
cheetah <thcheetah@gmail.com>
2012-06-22 05:35:02 +0600

    ld_p16f4 module added.

---
+++ modules/ld_p16f4.cpp
@@ -0,0 +1,64 @@
+#include "ld_p16f4.h"
+
+void ld_p16f4::init() {
+  Led::SetConfiguration(Led::Out);
+}
+
+void ld_p16f4::enable() {
+  PinOE::Clear();
+}
+
+void ld_p16f4::disable() {
+  PinOE::Set();
+}
+
+void ld_p16f4::shift(uint8_t state) {
+  int i;//, j, k;
+
+  switch(state) {
+    case 0:
+      LedData::Write<0>();     // 0b000000xxx
+      break;
+
+    case 1:
+      LedData::Write<0x18>();  // 0b000011xxx    
+      break;
+
+    case 2:
+      LedData::Write<0x60>(); // 0b001100xxx
+      break;
+
+    case 3:
+      LedData::Write<0x78>(); // 0b001111xxx
+      break;
+
+    case 4:
+      LedData::Write<0x180>(); // 0b110000xxx
+      break;
+
+    case 5:
+      LedData::Write<0x198>(); // 0b110011xxx
+      break;
+
+    case 6:
+      LedData::Write<0x1E0>(); // 0b111100xxx
+      break;
+
+    case 7:
+      LedData::Write<0x1F8>(); // 0b111111xxx
+      break;
+
+  }
+
+  for(i = 0; i < 64; i++) {
+    PinCLK::Set();
+    PinCLK::Clear();
+  }
+
+  PinLAT::Set();
+  PinLAT::Clear();
+}
+
+void ld_p16f4::test() {
+  _delay_ms(5);
+}
---
+++ modules/ld_p16f4.h
@@ -0,0 +1,39 @@
+#include <math.h>
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include <iopins.h>
+#include <timers.h>
+#include <pinlist.h>
+
+#include "led_iface.h"
+
+using namespace Mcucpp::IO;
+
+class ld_p16f4: public ILed {
+public:
+  void init();
+  void shift(uint8_t state);
+  void test();
+  void enable();
+  void disable();
+
+private:
+  
+  typedef PinList<Pc0, Pc1, Pc2, Pd0, Pd1, Pd2, Pd3, Pd4, Pd5> Led;
+
+  typedef Led::Slice<3, 6> LedData;
+
+  typedef Led::Pin<0>  PinOE;
+  typedef Led::Pin<1>  PinCLK;
+  typedef Led::Pin<2>  PinLAT;
+
+  typedef Led::Pin<3>  PinR1;
+  typedef Led::Pin<4>  PinR2;
+  typedef Led::Pin<5>  PinG1;
+  typedef Led::Pin<6>  PinG2;
+  typedef Led::Pin<7>  PinB1;
+  typedef Led::Pin<8>  PinB2;
+
+};
